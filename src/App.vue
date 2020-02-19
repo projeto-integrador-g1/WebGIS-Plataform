9192e2fd0e9cc9ad8cd28b810cc49b7ccd19f980
fabio rodrigues <fabiordrig@gmail.com>
2020-02-19 00:06:34 -0300

    feat: initial template with dependecies

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-app>
+    <core-app-bar />
+
+    <core-drawer />
+
+    <core-view />
+
+    <core-footer />
+  </v-app>
+</template>
+
+<script>
+  export default {
+    components: {
+      CoreDrawer: () => import('@/components/core/Drawer'),
+      CoreFooter: () => import('@/components/core/Footer'),
+      CoreAppBar: () => import('@/components/core/AppBar'),
+      CoreView: () => import('@/components/core/View')
+    }
+  }
+</script>
