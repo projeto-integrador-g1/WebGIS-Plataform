e938202eb7d8fcf4dcf1c4e71307a42f83b269cb
LuisBelo <luzaidinbelo@gmail.com>
2020-06-14 12:42:00 -0300

    refactor: add SceneParameters and ImagesCatalog modals

--- src/views/maps/MapBoxComponent.vue
+++ src/views/maps/MapBoxComponent.vue
@@ -26,29 +26,87 @@
           @map-init="initialized"
         />
       </v-card>
-      <v-row>
-        <v-col>
-          <v-card shaped>
-            <v-card-title>
-              <v-subheader>
-                Nivel de cobertura de nuvens
-              </v-subheader>
-              <v-slider v-model="slider" thumb-label="always"></v-slider>
-            </v-card-title>
-            <v-card-text>
-              <v-date-picker
-                :height="100"
-                :locale="locale"
-                v-model="date"
-                show-current
-              >
-              </v-date-picker>
+
+      <v-row justify="center">
+        <v-dialog v-model="showDialog" scrollable max-width="800px">
+          <v-card>
+            <h1 class="font-weight-bold text-center">Parâmetros</h1>
+            <v-divider></v-divider>
+            <v-card-text style="height: 600px;">
+              <h2>
+                Satélite
+              </h2>
+              <v-radio-group v-model="satelite" row>
+                <v-radio class="font-weight-black" label="Sentinel" value="sentinel"></v-radio>
+                <v-radio class="font-weight-black" label="Landsat" value="landsat"></v-radio>
+                <v-radio class="font-weight-black" label="Ambos" value="ambos"></v-radio>
+              </v-radio-group>
+              <v-divider></v-divider>
+
+              <h2 class="mb-10">
+                Nível de cobertura de nuvem
+              </h2>
+              <v-slider v-model="slider" thumb-label="always"  prepend-icon="mdi-cloud"></v-slider>
+              <v-divider></v-divider>
+
+              <h2>
+                Período de data das imagens
+              </h2>
+              <v-container fluid>
+                <v-row>
+                  <v-col cols="6" >
+                    <v-date-picker v-model="date" range locale="pt-br" show-current></v-date-picker>
+                  </v-col>
+                  <v-col cols="6">
+                    <v-text-field v-model="dateRangeText" label="Período entre as datas" prepend-icon="mdi-event" readonly></v-text-field>
+                  </v-col>
+                </v-row>
+              </v-container>
             </v-card-text>
+            <v-divider></v-divider>
             <v-card-actions>
-              <v-btn color="primary"> Realizar pesquisa</v-btn>
+              <v-btn color="blue darken-1" text @click="closeSceneParameters">Cancelar</v-btn>
+              <v-btn color="blue darken-1" text @click="validateSceneParameters">Pesquisar</v-btn>
             </v-card-actions>
           </v-card>
-        </v-col>
+        </v-dialog>
+      </v-row>
+
+
+      <v-row justify="center">
+        <v-dialog v-model="showCataloImages" scrollable max-width="800px">
+          <v-card height="100%">
+            <h1 class="font-weight-bold text-center">Catálogo de Imagens</h1>
+            <v-divider></v-divider>
+            <v-row>
+              <v-col v-for="(links, index) in catalogoLinks" :key="index" cols="6" sm="4">
+                <v-card>
+                  <v-img
+                    :src="links.href"
+                    :lazy-src="links.href"
+                    aspect-ratio="1"
+                    class="grey lighten-2"
+                  ></v-img>
+                  <v-card-text class="text--primary">
+                    <div> <b>Satélite:</b> {{links.collection}}</div>
+                    <div> <b>Cobertura de Nuvem:</b> {{links.cloud_cover}}</div>
+                    <div> <b>Data:</b> {{links.datetime}}</div>
+                    <div>
+                      <v-checkbox justify='center' v-model="imagemCatalogo"  
+                      :value="links.scene_id">
+                      </v-checkbox>
+                    </div>
+                  </v-card-text>
+                </v-card>
+              </v-col>
+            </v-row>
+            <v-divider></v-divider>
+            <v-card-actions>
+              <v-btn color="blue darken-1" text @click="closeCatalogoImages">Cancelar</v-btn>
+              <v-btn color="blue darken-1" text @click="validadeCatalogoImages">Pesquisar</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
       </v-row>
     </v-container>
   </div>
@@ -58,58 +116,224 @@
 import Mapbox from "mapbox-gl-vue";
 
 export default {
-  components: { Mapbox },
+  components: { Mapbox},
   data() {
     return {
-      coordinates: {
+      sceneParameters: {
         user_id: "5ecf7abcd91aa079f8b551f4",
-        geo_coord: []
+        geo_coord: [],
+        satelite: "",
+        date: [],
+        coberturaNuvem: 0,
       },
-      locale: "pt-br",
-      date: null,
+      date: [],
       files: [],
-      slider: null
+      satelite: '',
+      slider: null,
+      showDialog: false,
+      Draw: null,
+      showCataloImages: false,
+      imagemCatalogo: [],
+      catalogoLinks: {
+      "1": {
+        "collection": "landsat-8-l1",
+        "datetime": "2015-01-02T15:11:12.155190+00:00",
+        "cloud_cover": 7,
+        "eo:column": "010",
+        "eo:row": "022",
+        "href": "https://s3-us-west-2.amazonaws.com/landsat-pds/L8/010/022/LC80100222015002LGN00/LC80100222015002LGN00_thumb_large.jpg",
+        "scene_id": "LC80100222015002LGN00"
+      },
+      "2": {
+        "collection": "landsat-8-l1",
+        "datetime": "2015-02-19T15:10:59.052605+00:00",
+        "cloud_cover": 0,
+        "eo:column": "010",
+        "eo:row": "022",
+        "href": "https://s3-us-west-2.amazonaws.com/landsat-pds/L8/010/022/LC80100222015050LGN00/LC80100222015050LGN00_thumb_large.jpg",
+        "scene_id": "LC80100222015050LGN00"
+      },
+      "3": {
+        "collection": "landsat-8-l1",
+        "datetime": "2015-03-07T15:10:51.827001+00:00",
+        "cloud_cover": 15,
+        "eo:column": "010",
+        "eo:row": "022",
+        "href": "https://s3-us-west-2.amazonaws.com/landsat-pds/L8/010/022/LC80100222015066LGN00/LC80100222015066LGN00_thumb_large.jpg",
+        "scene_id": "LC80100222015066LGN00"
+      },
+      "4": {
+      "cloud_cover": 0,
+      "collection": "landsat-8-l1",
+      "column": "010",
+      "datetime": "2014-12-17T15:11:17.451083+00:00",
+      "href": "https://s3-us-west-2.amazonaws.com/landsat-pds/L8/010/022/LC80100222014351LGN00/LC80100222014351LGN00_thumb_large.jpg",
+      "row": "022",
+      "scene_id": "LC80100222014351LGN00"
+    },
+    "5": {
+      "cloud_cover": 6,
+      "collection": "landsat-8-l1",
+      "column": "010",
+      "datetime": "2014-12-01T15:11:21.658082+00:00",
+      "href": "https://s3-us-west-2.amazonaws.com/landsat-pds/L8/010/022/LC80100222014335LGN00/LC80100222014335LGN00_thumb_large.jpg",
+      "row": "022",
+      "scene_id": "LC80100222014335LGN00"
+    },
+    "6": {
+      "cloud_cover": 7,
+      "collection": "landsat-8-l1",
+      "column": "010",
+      "datetime": "2014-08-11T15:11:13.106201+00:00",
+      "href": "https://s3-us-west-2.amazonaws.com/landsat-pds/L8/010/022/LC80100222014223LGN00/LC80100222014223LGN00_thumb_large.jpg",
+      "row": "022",
+      "scene_id": "LC80100222014223LGN00"
+    },
+  "7": {
+    "cloud_cover": 9,
+    "collection": "landsat-8-l1",
+    "column": "010",
+    "datetime": "2014-06-08T15:10:50.604282+00:00",
+    "href": "https://s3-us-west-2.amazonaws.com/landsat-pds/L8/010/022/LC80100222014159LGN00/LC80100222014159LGN00_thumb_large.jpg",
+    "row": "022",
+    "scene_id": "LC80100222014159LGN00"
+  },
+  "8": {
+    "cloud_cover": 1,
+    "collection": "landsat-8-l1",
+    "column": "010",
+    "datetime": "2013-08-24T15:13:12.820512+00:00",
+    "href": "https://s3-us-west-2.amazonaws.com/landsat-pds/L8/010/022/LC80100222013236LGN00/LC80100222013236LGN00_thumb_large.jpg",
+    "row": "022",
+    "scene_id": "LC80100222013236LGN00"
+  },
+  "9": {
+    "cloud_cover": 9,
+    "collection": "landsat-8-l1",
+    "column": "010",
+    "datetime": "2015-12-20T15:11:18.775128+00:00",
+    "href": "https://s3-us-west-2.amazonaws.com/landsat-pds/L8/010/022/LC80100222015354LGN00/LC80100222015354LGN00_thumb_large.jpg",
+    "row": "022",
+    "scene_id": "LC80100222015354LGN00"
+  },
+  "10": {
+    "cloud_cover": 6,
+    "collection": "landsat-8-l1",
+    "column": "010",
+    "datetime": "2016-02-22T15:11:03.852525+00:00",
+    "href": "https://s3-us-west-2.amazonaws.com/landsat-pds/L8/010/022/LC80100222016053LGN00/LC80100222016053LGN00_thumb_large.jpg",
+    "row": "022",
+    "scene_id": "LC80100222016053LGN00"
+  },
+  "11": {
+    "cloud_cover": 14,
+    "collection": "landsat-8-l1",
+    "column": "010",
+    "datetime": "2016-03-25T15:10:53.923294+00:00",
+    "href": "https://s3-us-west-2.amazonaws.com/landsat-pds/L8/010/022/LC80100222016085LGN00/LC80100222016085LGN00_thumb_large.jpg",
+    "row": "022",
+    "scene_id": "LC80100222016085LGN00"
+  },}
     };
   },
   methods: {
     initialized(map) {
       let vm = this;
 
-      const Draw = new MapboxDraw({
+      this.Draw = new MapboxDraw({
         displayControlsDefault: false,
         controls: {
           polygon: true,
           trash: true
         }
       });
-      map.addControl(Draw);
+      map.addControl(this.Draw);
 
       map.on("draw.create", function(e) {
-        //console.log(e.features);
         console.log("Novo Polígono");
-
+        vm.showDialog = true;
+        vm.cleanSceneParameters();
+        vm.Draw.deleteAll();
+        
         for (var i = 0; i < e.features[0].geometry.coordinates[0].length; i++) {
           // A ultima coordenada e igual a primeira.
           var coordenada = [];
           coordenada.push(e.features[0].geometry.coordinates[0][i][1]); // latitude
           coordenada.push(e.features[0].geometry.coordinates[0][i][0]); // longitude
-          vm.coordinates.geo_coord.push(coordenada.toString());
+          vm.sceneParameters.geo_coord.push(coordenada.toString());
         }
-        vm.enviarCoordenadas();
+        // vm.enviarCoordenadas();
       });
     },
 
+    validateSceneParameters(){
+        if(!this.satelite || !this.date){
+          console.log('Nao Existe dado para satelite, data e nuvem');
+          console.log('Satelite: ' + this.satelite);
+          console.log('Data: ' + this.date);
+          console.log('Nuvem: ' + this.slider);
+
+          return
+        } else{
+          console.log('Existe dado para satelite, data e nuvem');
+          console.log('Satelite: ' + this.satelite);
+          console.log('Data: ' + this.date);
+          console.log('Nuvem: ' + this.slider);
+
+          this.sceneParameters.satelite = this.satelite;
+          this.sceneParameters.date = this.date;
+          this.sceneParameters.coberturaNuvem = this.slider;
+
+          this.showDialog = false;
+          this.showCataloImages = true;
+          console.log(this.sceneParameters);
+          //this.enviarCoordenadas();
+        }
+    },
+
+    validadeCatalogoImages(){
+      if(!this.imagemCatalogo){
+        console.log('Nao Existe dado para catalogo de imagens');
+        console.log('Catalo de Imagens: ' + this.imagemCatalogo);
+        return 
+      }
+
+      console.log('Catalogo de imagens escolhido: ' + this.imagemCatalogo);
+    },
+
+    closeSceneParameters(){
+      this.cleanSceneParameters();
+      this.showDialog = false;
+    },
+
+    closeCatalogoImages(){
+      this.showCataloImages = false;
+      this.showDialog = true;
+    },
+
+    cleanSceneParameters(){
+      this.date = [];
+      this.satelite = '';
+      this.slider = null;
+    },
+
     enviarCoordenadas() {
-      this.coordinates.geo_coord = this.coordinates.geo_coord.toString();
-      this.$store.dispatch("sendCoordinates", this.coordinates);
+      this.sceneParameters.geo_coord = this.sceneParameters.geo_coord.toString();
+      this.$store.dispatch("sendCoordinates", this.sceneParameters);
     }
-  }
+  },
+
+  computed: {
+      dateRangeText () {
+        return this.date.join(' ~ ')
+      },
+    },
 };
 </script>
 
 <style>
 #map {
   width: 100%;
-  height: 400px;
+  height: 600px;
 }
 </style>
