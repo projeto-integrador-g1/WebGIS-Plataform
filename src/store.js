8b1e61829ed07d73300d852dd531f74e82939d90
LuisBelo <luzaidinbelo@gmail.com>
2020-06-18 21:40:31 -0300

    refactor: add a return catalog array

--- src/store.js
+++ src/store.js
@@ -9,6 +9,7 @@ export default new Vuex.Store({
     barColor: 'rgba(0, 0, 0, .8), rgba(0, 0, 0, .8)',
     barImage: 'https://demos.creative-tim.com/material-dashboard/assets/img/sidebar-1.jpg',
     drawer: null,
+    catalog: {}
   },
   mutations: {
     SET_BAR_IMAGE (state, payload) {
@@ -17,11 +18,20 @@ export default new Vuex.Store({
     SET_DRAWER (state, payload) {
       state.drawer = payload
     },
+    SET_CATALOG (state, payload) {
+      state.catalog = payload
+    }
   },
   actions: {
     async sendCoordinates({ commit }, payload) {
-
-      return apiService.sendCoordinates(payload);
+      const imagesCatalog = await apiService.sendCoordinates(payload);
+      commit(SET_CATALOG, imagesCatalog);
     }
   },
+
+  getters: {
+    catalog: state => {
+      return state.catalog
+    }
+  }
 })
