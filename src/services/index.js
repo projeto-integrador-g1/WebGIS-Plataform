a68b849d5af83003e9a6d03f74b2701aa69cf1a7
LuisBelo <luzaidinbelo@gmail.com>
2020-07-06 20:53:29 -0300

    refactor: add method for send selected images catalog

--- src/services/index.js
+++ src/services/index.js
@@ -9,6 +9,16 @@ export const apiService = {
     });
     console.log(response.data);
     return response.data;
+  },
+
+  async sendImagesCatalog(data){
+    const response = await axios({
+      url: "http://127.0.0.1:4002/api/geoAI/",
+      method: "POST",
+      data: data
+    });
+    console.log('Api IA ',response.data);
+    return response.data;
   }
 };
 
