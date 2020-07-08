1f6c601ea987f67dad3d4f43d8bc3b93f679b3d0
LuisBelo <luzaidinbelo@gmail.com>
2020-07-08 07:59:48 -0300

    refactor: addd click outside

--- src/components/modal/ImagesCatalog.vue
+++ src/components/modal/ImagesCatalog.vue
@@ -1,140 +1,125 @@
 <template>
-  <div>
-    <v-row justify="center">
-      <v-dialog v-model="showImagesCatalog" scrollable max-width="800px">
-        <v-card height="100%">
-          <h1 class="font-weight-bold text-center">Catálogo de Imagens</h1>
-          <v-divider></v-divider>
-          <v-row
-            v-show="Object.keys(imagesCatalog).length == 0"
-            justify="center"
-            style="height: 600px;"
-          >
-            <div class="text-md-center ma-12" style="padding-top:200px;">
-              <v-progress-circular
-                indeterminate
-                color="primary"
-                size="60"
-              ></v-progress-circular>
-            </div>
-          </v-row>
-          <v-row v-show="Object.keys(imagesCatalog).length > 0">
-            <v-col
-              v-for="(image, index) in imagesCatalog"
-              :key="index"
-              cols="6"
-              sm="4"
-            >
-              <v-card>
-                <v-img
-                  :src="image.href"
-                  :lazy-src="image.href"
-                  aspect-ratio="1"
-                  class="grey lighten-2"
-                ></v-img>
-                <v-card-text class="text--primary">
-                  <div><b>Satélite:</b> {{ image.collection }}</div>
-                  <div><b>Cobertura de Nuvem:</b> {{ image.cloud_cover }}</div>
-                  <div><b>Data:</b> {{ image.datetime }}</div>
-                  <div>
-                    <v-checkbox
-                      justify="center"
-                      v-model="selectedImagesCatalog"
-                      :value="image.scene_id"
-                    >
-                    </v-checkbox>
-                  </div>
-                </v-card-text>
-              </v-card>
-            </v-col>
-          </v-row>
-          <v-divider></v-divider>
-          <v-card-actions>
-            <v-btn color="blue darken-1" text @click="closeImagesCatalog"
-              >Cancelar</v-btn
-            >
-            <v-btn color="blue darken-1" text @click="validateImagesCatalog"
-              >Pesquisar</v-btn
-            >
-          </v-card-actions>
-        </v-card>
-      </v-dialog>
-    </v-row>
-  </div>
+    <div>
+        <v-row justify="center">
+        <v-dialog v-model="showImagesCatalog" @click:outside='closeImagesCatalog' scrollable max-width="800px">
+          <v-card height="100%" >
+            <h1 class="font-weight-bold text-center">Catálogo de Imagens</h1>
+            <v-divider></v-divider>
+            <v-row v-show="Object.keys(imagesCatalog).length == 0" justify="center" style="height: 600px;">
+               <div class="text-md-center ma-12" style="padding-top:200px;">
+                    <v-progress-circular  
+                    indeterminate
+                    color="primary"
+                    size="60" 
+                    ></v-progress-circular>
+                </div>
+            </v-row>
+            <v-row v-show="Object.keys(imagesCatalog).length > 0">
+              <v-col  v-for="(image, index) in imagesCatalog" :key="index" cols="6" sm="4">
+                <v-card>
+                  <v-img
+                    :src="image.href"
+                    :lazy-src="image.href"
+                    aspect-ratio="1"
+                    class="grey lighten-2"
+                  ></v-img>
+                  <v-card-text class="text--primary">
+                    <div> <b>Satélite:</b> {{image.collection}}</div>
+                    <div> <b>Cobertura de Nuvem:</b> {{image.cloud_cover}}</div>
+                    <div> <b>Data:</b> {{image.datetime}}</div>
+                    <div>
+                      <v-checkbox justify='center' v-model="selectedImagesCatalog"  
+                      :value="image.scene_id">
+                      </v-checkbox>
+                    </div>
+                  </v-card-text>
+                </v-card>
+              </v-col>
+            </v-row>
+            <v-divider></v-divider>
+            <v-card-actions>
+              <v-btn color="blue darken-1" text @click="closeImagesCatalog">Cancelar</v-btn>
+              <v-btn color="blue darken-1" text @click="validateImagesCatalog">Pesquisar</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+      </v-row>
+    </div>
 </template>
 
 <script>
-export default {
-  name: "ImagesCatalog",
-  props: {
-    value: {
-      type: Boolean
+    export default {
+        name: 'ImagesCatalog',
+        props: {
+            value: {
+                type: Boolean
+            },
+        },
+
+        data(){
+            return{
+                selectedImagesCatalog: [],
+                catalog: {
+                  images: []
+                }
+            }
+        },
+
+        computed:{
+            showImagesCatalog: {
+                get(){
+                    return this.value;
+                },
+
+                set(value){
+                    this.$emit('input', value);
+                }
+            },
+
+            imagesCatalog(){
+                console.log('catalog images ', this.$store.getters.catalog);
+                return this.$store.getters.catalog
+            }
+        },
+
+        methods:{
+            validateImagesCatalog(){
+                if(!this.selectedImagesCatalog){
+                    console.log('Nao Existe dado para catalogo de imagens');
+                    console.log('Catalo de Imagens: ' + this.selectedImagesCatalog);
+                    return 
+                }
+
+                console.log('Catalogo de imagens escolhido: ' + this.selectedImagesCatalog);
+                this.catalog.images = [];
+                this.catalog.images = this.selectedImagesCatalog;
+                console.log(this.catalog.images, 'catalogo de images escolhido');
+                this.sendImagesCatalog();
+            },
+
+            closeImagesCatalog(){
+                if(this.imagesCatalog){
+                  this.cleanImagesCatalog();
+                  console.log('dados do catalog', this.imagesCatalog);
+                } else {
+                  console.log('Nao existe dados no images catalog');
+                }
+
+                this.showImagesCatalog = false;
+            },
+            
+            cleanImagesCatalog(){
+              this.$store.commit("SET_CATALOG", []);
+              this.catalog.images = [];
+            },
+
+            sendImagesCatalog(){
+              //this.$store.dispatch("sendImagesCatalog", this.catalog);
+            }
+        }
     }
-  },
-
-  data() {
-    return {
-      selectedImagesCatalog: [],
-      catalog: {
-        images: []
-      }
-    };
-  },
-
-  computed: {
-    showImagesCatalog: {
-      get() {
-        return this.value;
-      },
-
-      set(value) {
-        this.$emit("input", value);
-      }
-    },
-
-    imagesCatalog() {
-      console.log("catalog images ", this.$store.getters.catalog);
-      return this.$store.getters.catalog;
-    }
-  },
-
-  methods: {
-    validateImagesCatalog() {
-      if (!this.selectedImagesCatalog) {
-        console.log("Nao Existe dado para catalogo de imagens");
-        console.log("Catalo de Imagens: " + this.selectedImagesCatalog);
-        return;
-      }
-
-      console.log(
-        "Catalogo de imagens escolhido: " + this.selectedImagesCatalog
-      );
-      this.catalog.images = [];
-      this.catalog.images = this.selectedImagesCatalog;
-      console.log(this.catalog.images, "catalogo de images escolhido");
-      this.sendImagesCatalog();
-    },
-
-    closeImagesCatalog() {
-      this.showImagesCatalog = false;
-      if (this.imagesCatalog) {
-        this.cleanImagesCatalog();
-        console.log("dados do catalog", this.imagesCatalog);
-      } else {
-        console.log("Nao existe dados no images catalog");
-      }
-    },
-
-    cleanImagesCatalog() {
-      this.$store.commit("SET_CATALOG", []);
-      this.catalog.images = [];
-    },
-
-    sendImagesCatalog() {
-      this.$store.dispatch("sendImagesCatalog", this.catalog);
-    }
-  }
-};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+
+</style>
