091328cbf8f2e79b310a64e16f0666a50c268c9a
LuisBelo <luzaidinbelo@gmail.com>
2020-07-08 08:01:33 -0300

    refactor: add ShapeFile reader

--- src/views/maps/MapBoxComponent.vue
+++ src/views/maps/MapBoxComponent.vue
@@ -21,9 +21,11 @@
           }"
           @map-init="initialized"
         />
+        <input type="file" ref="myFile" @change="loadTextFromFile">
       </v-card>
 
-      <SceneParameters v-model="showSceneParameters" :coordinates="geo_coord" />
+      <SceneParameters v-model="showSceneParameters" :coordinates="geo_coord"/>
+      <LoadingShapeFile v-model="showLoadingShapeFile"/>
     </v-container>
   </div>
 </template>
@@ -31,20 +33,59 @@
 <script>
 import Mapbox from "mapbox-gl-vue";
 import SceneParameters from "../../components/modal/SceneParameters.vue";
-import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder";
-import "@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css";
+import LoadingShapeFile from "../../components/modal/LoadingShapeFile.vue";
+import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
+import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 
 export default {
-  components: { Mapbox, SceneParameters },
+  components: { Mapbox, SceneParameters, LoadingShapeFile},
   data() {
     return {
       geo_coord: [],
-      files: [],
+      file: null,
       showSceneParameters: false,
-      Draw: null
-    };
+      showLoadingShapeFile: false,
+      Draw: null,
+    }
   },
   methods: {
+    loadTextFromFile() {
+      let vm = this;
+      console.log('selected a file');
+      console.log(this.$refs.myFile.files[0]);
+      
+      let file = this.$refs.myFile.files[0];
+
+      if(file.name){
+        const splitName = file.name.split('.');
+        console.log('spliname', splitName);
+        if(splitName[splitName.length - 1] === 'shp'){
+          let reader = new FileReader();
+          reader.readAsText(file, "UTF-8");
+          reader.onload =  evt => {
+            this.text = evt.target.result;
+            vm.file = this.text;
+            vm.showLoadingShapeFile = true;
+            console.log('shapefile', vm.file);
+            //vm.sendShapeFile();
+
+          }
+          reader.onerror = evt => {
+            console.error(evt);
+            }
+        } else{
+          console.log('nao e arquivo shapefile');
+          return;
+        }
+      } else{
+        console.log('nao e arquivo shapefile');
+        return;
+      }
+      //if(!file || file.type !== 'text/plain') return;
+      
+      // Credit: https://stackoverflow.com/a/754398/52160
+    },
+
     initialized(map) {
       let vm = this;
 
@@ -59,45 +100,42 @@ export default {
 
       var coordinatesGeocoder = function(query) {
         // match anything which looks like a decimal degrees coordinate pair
-        var matches = query.match(
-          /^[ ]*(?:Lat: )?(-?\d+\.?\d*)[, ]+(?:Lng: )?(-?\d+\.?\d*)[ ]*$/i
-        );
+        var matches = query.match(/^[ ]*(?:Lat: )?(-?\d+\.?\d*)[, ]+(?:Lng: )?(-?\d+\.?\d*)[ ]*$/i);
         if (!matches) {
           return null;
         }
         var coord1 = Number(matches[1]);
         var coord2 = Number(matches[2]);
         var geocodes = [];
-
+        
         if (coord1 < -90 || coord1 > 90) {
           // must be lng, lat
           geocodes.push(vm.coordinateFeature(coord1, coord2));
         }
-
+        
         if (coord2 < -90 || coord2 > 90) {
           // must be lat, lng
           geocodes.push(vm.coordinateFeature(coord2, coord1));
         }
-
+        
         if (geocodes.length === 0) {
           // else could be either lng, lat or lat, lng
           geocodes.push(vm.coordinateFeature(coord1, coord2));
           geocodes.push(vm.coordinateFeature(coord2, coord1));
         }
-
+        
         return geocodes;
-      };
+      }
       const geocoder = new MapboxGeocoder({
-        accessToken:
-          "pk.eyJ1IjoibHVjaWFuby1zY3J1bSIsImEiOiJjazdkOGJ6YTQxY3Z4M2Vxcmd4NTBhNjBmIn0.vRDaTxND4t-C_b5XB4JLmA",
-        mapboxgl: Mapbox,
-        countries: "br",
-        marker: false,
-        localGeocoder: coordinatesGeocoder,
-        placeholder: "Procurar..."
+          accessToken: "pk.eyJ1IjoibHVjaWFuby1zY3J1bSIsImEiOiJjazdkOGJ6YTQxY3Z4M2Vxcmd4NTBhNjBmIn0.vRDaTxND4t-C_b5XB4JLmA",
+          mapboxgl: Mapbox,
+          countries: 'br',
+          marker: false,
+          localGeocoder: coordinatesGeocoder,
+          placeholder: 'Procurar...'
       });
 
-      map.addControl(geocoder, "top-left");
+      map.addControl(geocoder, 'top-left');
 
       map.on("draw.create", function(e) {
         console.log("Novo Polígono");
@@ -105,7 +143,7 @@ export default {
         vm.geo_coord = [];
 
         vm.Draw.deleteAll();
-
+        
         for (var i = 0; i < e.features[0].geometry.coordinates[0].length; i++) {
           // A ultima coordenada e igual a primeira.
           var coordenada = [];
@@ -119,20 +157,25 @@ export default {
       });
     },
 
-    coordinateFeature(lng, lat) {
+    coordinateFeature(lng, lat){
       return {
         center: [lng, lat],
         geometry: {
-          type: "Point",
+          type: 'Point',
           coordinates: [lng, lat]
         },
-        place_name: "Lat: " + lat + " Lng: " + lng,
-        place_type: ["coordinate"],
+        place_name: 'Lat: ' + lat + ' Lng: ' + lng,
+        place_type: ['coordinate'],
         properties: {},
-        type: "Feature"
+        type: 'Feature'
       };
+    },
+
+    sendShapeFile(){
+      this.$store.dispatch("sendShapeFile", this.file);
     }
-  }
+  },
+
 };
 </script>
 
