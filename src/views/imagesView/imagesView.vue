d40d8a128cf7eacdfb6d8caefb2a3f5839abb583
LuisBelo <luzaidinbelo@gmail.com>
2020-07-12 16:33:51 -0300

    refactor: component

--- src/views/imagesView/imagesView.vue
+++ src/views/imagesView/imagesView.vue
@@ -1,22 +1,31 @@
 <template>
-  <v-expansion-panels>
-    <v-expansion-panel v-for="(item, i) in 2" :key="i">
-      <v-expansion-panel-header>Resultado</v-expansion-panel-header>
+  <v-expansion-panels >
+    <div v-if="!showResults" class="text-md-center ma-12" style="padding-top:200px;">
+        <v-progress-circular  
+        indeterminate
+        color="primary"
+        size="60" 
+        ></v-progress-circular>
+    </div>
+    <v-expansion-panel v-else>
+      <v-expansion-panel-header>Resultados</v-expansion-panel-header>
       <v-expansion-panel-content>
         <v-col cols="12" sm="6" offset-sm="3">
           <v-card>
             <v-container fluid>
-              <v-row>
-                <v-col>
-                  <v-carousel>
-                    <v-carousel-item
-                      v-for="(item, i) in imgurLinks"
-                      :key="i"
-                      :src="item"
-                      reverse-transition="fade-transition"
-                      transition="fade-transition"
-                    ></v-carousel-item>
-                  </v-carousel>
+              <v-row >
+                <v-col  v-for="(image, index) in imgurLinks" :key="index" cols="12" sm="6" offset-sm="3">
+                  <v-card>
+                    <v-img
+                      :src="image"
+                      :lazy-src="image"
+                      aspect-ratio="1"
+                      class="grey lighten-2"
+                    ></v-img>
+                    <v-card-text class="text--primary">
+                      <div> <a target="_blank" :href="image" >Download</a></div>
+                    </v-card-text>
+                  </v-card>
                 </v-col>
               </v-row>
             </v-container>
@@ -30,13 +39,31 @@
 export default {
   data() {
     return {
-      imgurLinks: [
-        "https://i.imgur.com/KLoDw18.png",
-        "https://i.imgur.com/rqT4Bmx.png",
-        "https://i.imgur.com/qpkWowI.jpg",
-        "https://i.imgur.com/Xq9UTpP.png"
-      ]
+      imgurLinks: []
     };
+  },
+
+  computed:{
+    imagesUserTalhoes(){
+      return this.$store.getters.userTalhoes
+    },
+
+    showResults(){
+      if(Object.keys(this.imagesUserTalhoes).length > 0){
+        this.splitImages();
+        return true;
+      } else{
+        return false;
+      }
+    }
+  },
+
+  methods:{
+    splitImages(){
+      const splitLink = this.imagesUserTalhoes.user_imgs.split(';');
+      this.imgurLinks = splitLink;
+      console.log('links de resposta', this.imgurLinks);
+    }
   }
 };
 </script>
